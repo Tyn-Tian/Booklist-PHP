e6f3102aab7ec9edfab52f1f435d44678a2ddce7
Christian <kristian2015ok@gmail.com>
2024-04-26 15:55:26 +0700

    Update: App

--- App.php
+++ App.php
@@ -1,7 +1,20 @@
 <?php
 
-require_once "./View/ViewShowBookList.php";
+
+use Repository\BooklistRepositoryImpl;
+use Service\BooklistServiceImpl;
+use View\BooklistView;
+
+require_once "./Repository/BooklistRepository.php";
+require_once "./Service/BooklistService.php";
+require_once "./View/BooklistView.php";
+require_once "./Helper/InputHelper.php";
+require_once "./Entity/Booklist.php";
 
 echo "Aplikasi Booklist" . PHP_EOL;
 
-viewShowBookList();
+$booklistRepository = new BooklistRepositoryImpl();
+$booklistService = new BooklistServiceImpl($booklistRepository);
+$booklistView = new BooklistView($booklistService);
+
+$booklistView->showBooklist();
