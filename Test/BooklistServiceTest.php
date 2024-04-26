13f0cd9084426a4367862b8c66b9278466479c6b
Christian <kristian2015ok@gmail.com>
2024-04-26 16:17:11 +0700

    Update: booklist service test connect database

--- Test/BooklistServiceTest.php
+++ Test/BooklistServiceTest.php
@@ -4,6 +4,7 @@ require_once "./Entity/Booklist.php";
 require_once "./Repository/BooklistRepository.php";
 require_once "./Service/BooklistService.php";
 require_once "./Helper/AddBooklistHelper.php";
+require_once "./Config/Database.php";
 
 use Entity\Booklist;
 use Repository\BooklistRepositoryImpl;
@@ -12,19 +13,19 @@ use Helper\AddBooklistHelper;
 
 function testShowBooklist(): void
 {
-    $booklistRepository = new BooklistRepositoryImpl();
-    $booklistRepository->booklist[1] = new Booklist("Belajar PHP");
-    $booklistRepository->booklist[2] = new Booklist("Belajar PHP OOP");
-    $booklistRepository->booklist[3] = new Booklist("Belajar PHP 8");
-
+    $connection = \Config\Database::getConnection();
+    $booklistRepository = new BooklistRepositoryImpl($connection);
     $booklistService = new BooklistServiceImpl($booklistRepository);
+
+    AddBooklistHelper::addBooklist($booklistService);
 
     $booklistService->showBooklist();
 }
 
 function testAddBooklist(): void 
 {
-    $booklistRepository = new BooklistRepositoryImpl();
+    $connection = \Config\Database::getConnection();
+    $booklistRepository = new BooklistRepositoryImpl($connection);
 
     $booklistService = new BooklistServiceImpl($booklistRepository);
     AddBooklistHelper::addBooklist($booklistService);
@@ -34,7 +35,8 @@ function testAddBooklist(): void
 
 function testRemoveBooklist(): void 
 {
-    $booklistRepository = new BooklistRepositoryImpl();
+    $connection = \Config\Database::getConnection();
+    $booklistRepository = new BooklistRepositoryImpl($connection);
 
     $booklistService = new BooklistServiceImpl($booklistRepository);
     AddBooklistHelper::addBooklist($booklistService);
