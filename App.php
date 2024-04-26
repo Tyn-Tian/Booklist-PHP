8d2f03e8c02e61020da4845301d72fef06556033
Christian <kristian2015ok@gmail.com>
2024-04-26 16:17:50 +0700

    Update: app connect to database

--- App.php
+++ App.php
@@ -10,10 +10,12 @@ require_once "./Service/BooklistService.php";
 require_once "./View/BooklistView.php";
 require_once "./Helper/InputHelper.php";
 require_once "./Entity/Booklist.php";
+require_once "./Config/Database.php";
 
 echo "Aplikasi Booklist" . PHP_EOL;
 
-$booklistRepository = new BooklistRepositoryImpl();
+$connection = \Config\Database::getConnection();
+$booklistRepository = new BooklistRepositoryImpl($connection);
 $booklistService = new BooklistServiceImpl($booklistRepository);
 $booklistView = new BooklistView($booklistService);
 
