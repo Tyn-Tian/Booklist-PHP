2045be7bd1ca8bade0b0152c36c24ebd68833241
Christian <kristian2015ok@gmail.com>
2024-04-26 16:13:16 +0700

    Update: Booklist

--- Entity/Booklist.php
+++ Entity/Booklist.php
@@ -4,9 +4,21 @@ namespace Entity;
 
 class Booklist
 {
+    private int $id;
+
     public function __construct(
         private string $book = ""
     ) {
+    }
+
+    public function setId(int $id): void 
+    {
+        $this->id = $id;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
     }
 
     public function getBook(): string
