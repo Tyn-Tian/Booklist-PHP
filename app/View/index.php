beadd1bde71440330615c25ded9a3fccc19de4ff
Christian <kristian2015ok@gmail.com>
2024-05-07 14:46:18 +0700

    Create: View

---
+++ app/View/index.php
@@ -0,0 +1,35 @@
+<div class="container py-5">
+        <h1 class="text-center fw-bold display-4">Booklist PHP</h1>
+
+        <div class="container py-3">
+            <div class="row text-center">
+                <div class="col-12 pb-5">
+                    <button type="button" class="btn btn-outline-success">Add New Booklist</button>
+                </div>
+                <div class="col-xl-4 col-md-6 col-12 p-2">
+                    <div class="card mb-1">
+                        <div class="card-body">
+                            This is some text within a card body.
+                            <a href="#" class="text-danger text-decoration-none">Delete</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-6 col-12 p-2">
+                    <div class="card mb-1">
+                        <div class="card-body">
+                            This is some text within a card body.
+                            <a href="#" class="text-danger text-decoration-none">Delete</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-md-6 col-12 p-2">
+                    <div class="card mb-1">
+                        <div class="card-body">
+                            This is some text within a card body.
+                            <a href="#" class="text-danger text-decoration-none">Delete</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
