2b8ffc76bc33d2ddc074ef003038084092804fd0
Christian <kristian2015ok@gmail.com>
2024-05-07 20:17:02 +0700

    Create: Booklist Service

---
+++ app/Model/AddBooklistResponse.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace BooklistPhp\Model;
+
+use BooklistPhp\Domain\Booklist;
+
+class AddBooklistResponse
+{
+    public ?Booklist $booklist;
+}
--- app/Service/BooklistService.php
+++ app/Service/BooklistService.php
@@ -2,44 +2,86 @@
 
 namespace BooklistPhp\Service;
 
-use BooklistPhp\Entity\Booklist;
+use BooklistPhp\Config\Database;
+use BooklistPhp\Domain\Booklist;
+use BooklistPhp\Exception\ValidationException;
+use BooklistPhp\Model\AddBooklistRequest;
+use BooklistPhp\Model\AddBooklistResponse;
 use BooklistPhp\Repository\BooklistRepository;
 
-interface BooklistServiceInterface
+// interface BooklistServiceInterface
+// {
+//     function showBooklist(): void;
+
+//     function addBooklist(string $book): void;
+
+//     function removeBooklist(int $number): void;
+// }
+
+// class BooklistService implements BooklistServiceInterface
+// {
+//     public function __construct(
+//         private BooklistRepository $booklistRepository
+//     ) {
+//     }
+
+//     public function showBooklist(): void
+//     {
+//         echo "Booklist" . PHP_EOL;
+//         $booklist = $this->booklistRepository->findAll();
+//         foreach ($booklist as $number => $value) {
+//             echo $value->getId(). ". " . $value->getBook() . PHP_EOL;
+//         }
+//     }
+
+//     function addBooklist(string $book): void
+//     {
+//         $booklist = new Booklist($book);
+//         $this->booklistRepository->save($booklist);
+//         echo "SUKSES MENAMBAH TODOLIST" . PHP_EOL;
+//     }
+
+//     function removeBooklist(int $number): void
+//     {
+//         echo $this->booklistRepository->remove($number) ? "SUKSES" : "GAGAL"; 
+//         echo " MENGHAPUS BOOKLIST" . PHP_EOL;
+//     }
+// }
+
+class BooklistService
 {
-    function showBooklist(): void;
+    private \PDO $connection;
 
-    function addBooklist(string $book): void;
-
-    function removeBooklist(int $number): void;
-}
-
-class BooklistService implements BooklistServiceInterface
-{
     public function __construct(
         private BooklistRepository $booklistRepository
     ) {
+        $this->connection = Database::getConnection();
     }
 
-    public function showBooklist(): void
+    public function addBooklist(AddBooklistRequest $request): ?AddBooklistResponse
     {
-        echo "Booklist" . PHP_EOL;
-        $booklist = $this->booklistRepository->findAll();
-        foreach ($booklist as $number => $value) {
-            echo $value->getId(). ". " . $value->getBook() . PHP_EOL;
+        $this->validateAddBooklistRequest($request);
+
+        try {
+            $booklist = new Booklist();
+            $booklist->book = $request->book;
+
+            $this->booklistRepository->save($booklist);
+            $booklist->id = $this->connection->lastInsertId();
+
+            $response = new AddBooklistResponse();
+            $response->booklist = $booklist;
+
+            return $response;
+        } catch (\Exception $exception) {
+            throw $exception;
         }
     }
 
-    function addBooklist(string $book): void
+    private function validateAddBooklistRequest(AddBooklistRequest $request)
     {
-        $booklist = new Booklist($book);
-        $this->booklistRepository->save($booklist);
-        echo "SUKSES MENAMBAH TODOLIST" . PHP_EOL;
-    }
-
-    function removeBooklist(int $number): void
-    {
-        echo $this->booklistRepository->remove($number) ? "SUKSES" : "GAGAL"; 
-        echo " MENGHAPUS BOOKLIST" . PHP_EOL;
+        if ($request->book == null || trim($request->book) == "") {
+            throw new ValidationException("Book name cannot blank");
+        }
     }
 }
