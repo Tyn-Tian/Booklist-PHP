b73bb134c50f3645690a0d51e52cdff250fa6bc9
Christian <kristian2015ok@gmail.com>
2024-05-08 10:30:56 +0700

    Create: remove booklist repository

--- app/Repository/BooklistRepository.php
+++ app/Repository/BooklistRepository.php
@@ -2,77 +2,13 @@
 
 namespace BooklistPhp\Repository;
 
-// use BooklistPhp\Entity\Booklist;
-
-// interface BooklistRepositoryInterface
-// {
-//     function findAll(): array;
-
-//     function save(Booklist $booklist): void;
-
-//     function remove(int $number): bool;
-// }
-
-// class BooklistRepository implements BooklistRepositoryInterface
-// {
-//     public array $booklist = array();
-
-//     public function __construct(
-//         private \PDO $connection
-//     ) {
-//     }
-
-//     public function findAll(): array
-//     {
-//         $sql = "SELECT id, book FROM booklist";
-//         $result = $this->connection->prepare($sql);
-//         $result->execute();
-
-//         $resultArray = [];
-
-//         foreach ($result as $row) {
-//             $booklist = new Booklist();
-//             $booklist->setId($row['id']);
-//             $booklist->setBook($row['book']);
-
-//             $resultArray[] = $booklist;
-//         }
-
-//         return $resultArray;
-//     }
-
-//     public function save(Booklist $booklist): void
-//     {
-//         $sql = "INSERT INTO booklist(book) VALUES (?)";
-//         $result = $this->connection->prepare($sql);
-//         $result->execute([$booklist->getBook()]);
-//     }
-
-//     public function remove(int $number): bool
-//     {
-//         $sql = "SELECT id FROM booklist WHERE id = ?";
-//         $result = $this->connection->prepare($sql);
-//         $result->execute([$number]);
-
-//         if ($result->fetch()) {
-//             $sql = "DELETE FROM booklist WHERE id = ?";
-//             $result = $this->connection->prepare($sql);
-//             $result->execute([$number]);
-//             return true;
-//         } else {
-//             return false;
-//         }
-//     }
-// }
-
 use BooklistPhp\Domain\Booklist;
 
 class BooklistRepository
 {
     public function __construct(
         private \PDO $connection
-    )
-    {
+    ) {
     }
 
     public function save(Booklist $booklist): void
@@ -100,7 +36,7 @@ class BooklistRepository
         }
     }
 
-    public function deleteAll() 
+    public function deleteAll()
     {
         $this->connection->exec("DELETE FROM booklist");
     }
@@ -111,5 +47,19 @@ class BooklistRepository
         $statement->execute();
 
         return $statement->fetchAll();
+    }
+
+    public function remove(string $id): bool
+    {
+        $statement = $this->connection->prepare("SELECT id FROM booklist WHERE id = ?");
+        $statement->execute([$id]);
+
+        if ($statement->fetch()) {
+            $statement = $this->connection->prepare("DELETE FROM booklist WHERE id = ?");
+            $statement->execute([$id]);
+            return true;
+        }
+
+        return false;
     }
 }
