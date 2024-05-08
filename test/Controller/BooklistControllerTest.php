a8e8a1dd34ced505320a5dac2be1f09017aff978
Christian <kristian2015ok@gmail.com>
2024-05-08 09:16:52 +0700

    Test: add booklist controller

--- test/Controller/BooklistControllerTest.php
+++ test/Controller/BooklistControllerTest.php
@@ -2,15 +2,24 @@
 
 namespace BooklistPhp\Controller;
 
+require_once __DIR__ . "/../helper/helper.php";
+
+use BooklistPhp\Config\Database;
+use BooklistPhp\Repository\BooklistRepository;
 use PHPUnit\Framework\TestCase;
 
 class BooklistControllerTest extends TestCase
 {
     private BooklistController $booklistController;
+    private BooklistRepository $booklistRepository;
 
     protected function setUp(): void
     {
+        $this->booklistRepository = new BooklistRepository(Database::getConnection());
         $this->booklistController = new BooklistController();
+
+        $this->booklistRepository->deleteAll();
+        putenv("mode=test");
     }
 
     public function testIndex()
@@ -19,5 +28,37 @@ class BooklistControllerTest extends TestCase
 
         $this->expectOutputRegex("[Booklist PHP]");
         $this->expectOutputRegex("[Add New Booklist]");
+    }
+
+    public function testAddBooklist()
+    {
+        $this->booklistController->addBooklist();
+
+        $this->expectOutputRegex("[Add New Booklist]");
+        $this->expectOutputRegex("[Form]");
+        $this->expectOutputRegex("[Book Title]");
+        $this->expectOutputRegex("[Add New Book]");
+    }
+
+    public function testPostAddBooklistSuccess()
+    {
+        $_POST['book'] = "Atomic Habits";
+
+        $this->booklistController->postAddBooklist();
+
+        $this->expectOutputRegex("[Location: /]");
+    }
+
+    public function testPostAddBooklistValidationError()
+    {
+        $_POST['book'] = "";
+
+        $this->booklistController->postAddBooklist();
+
+        $this->expectOutputRegex("[Book name cannot blank]");
+        $this->expectOutputRegex("[Add New Booklist]");
+        $this->expectOutputRegex("[Form]");
+        $this->expectOutputRegex("[Book Title]");
+        $this->expectOutputRegex("[Add New Book]");
     }
 }
