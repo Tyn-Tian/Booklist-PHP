c34882d8a917aab8848301b7989e2f3cc527c001
Christian <kristian2015ok@gmail.com>
2024-05-08 10:45:29 +0700

    Create: delete booklist service

--- app/Service/BooklistService.php
+++ app/Service/BooklistService.php
@@ -9,45 +9,6 @@ use BooklistPhp\Model\AddBooklistRequest;
 use BooklistPhp\Model\AddBooklistResponse;
 use BooklistPhp\Repository\BooklistRepository;
 
-// interface BooklistServiceInterface
-// {
-//     function showBooklist(): void;
-
-//     function addBooklist(string $book): void;
-
-//     function removeBooklist(int $number): void;
-// }
-
-// class BooklistService implements BooklistServiceInterface
-// {
-//     public function __construct(
-//         private BooklistRepository $booklistRepository
-//     ) {
-//     }
-
-//     public function showBooklist(): void
-//     {
-//         echo "Booklist" . PHP_EOL;
-//         $booklist = $this->booklistRepository->findAll();
-//         foreach ($booklist as $number => $value) {
-//             echo $value->getId(). ". " . $value->getBook() . PHP_EOL;
-//         }
-//     }
-
-//     function addBooklist(string $book): void
-//     {
-//         $booklist = new Booklist($book);
-//         $this->booklistRepository->save($booklist);
-//         echo "SUKSES MENAMBAH TODOLIST" . PHP_EOL;
-//     }
-
-//     function removeBooklist(int $number): void
-//     {
-//         echo $this->booklistRepository->remove($number) ? "SUKSES" : "GAGAL"; 
-//         echo " MENGHAPUS BOOKLIST" . PHP_EOL;
-//     }
-// }
-
 class BooklistService
 {
     private \PDO $connection;
@@ -91,5 +52,10 @@ class BooklistService
     public function showBooklist(): array
     {
         return $this->booklistRepository->findAll();
+    }
+
+    public function deleteBooklist(string $id): bool
+    {
+        return $this->booklistRepository->remove($id);
     }
 }
